c076f1520cf111b4d20e58b85ffe7b70dd3e12c6
artmkchmv <102462249+artmkchmv@users.noreply.github.com>
2024-11-22 14:03:17 +0400

    Update Lab3.h

--- Lab3/Lab3.h
+++ Lab3/Lab3.h
@@ -14,7 +14,52 @@ numerics::vector_f64 conjugate_grad(func_nd func, const numerics::vector_f64& x_
 numerics::vector_f64 newton_raphson(func_nd func, const numerics::vector_f64& x_start, const double eps = 1e-6, const int max_iters = 100);
 
 template<typename T>
-struct penalty_function;
+struct penalty_function {
+
+public:
+
+	using vect = numerics::numeric_vector_<T>;
+	using bound_func = std::function<T(const vect&)>;
+
+private:
+
+	std::list<bound_func> m_boundaries;
+	bound_func m_target;
+
+public:
+
+	penalty_function()
+		: m_target([](const vect& arg) {return vect::template dot(arg, arg); }) {}
+
+	penalty_function(const bound_func& target)
+		: m_target(target) {}
+
+	const bound_func& get_target() const {
+		return m_target;
+	}
+
+	void set_target(const bound_func& func) {
+		m_target = func;
+	}
+
+	void add_bound(const bound_func& func) {
+		m_boundaries.push_back(func);
+	}
+
+	void del_bound(const bound_func& func) {
+		m_boundaries.remove(func);
+	}
+
+	T operator()(const vect& arg) const {
+		T value = m_target(arg);
+
+		for (auto const& bound : m_boundaries) {
+			value += bound(arg);
+		}
+
+		return value;
+	}
+};
 
 typedef penalty_function<float> penalty_f32;
 typedef penalty_function<double> penalty_f64;
