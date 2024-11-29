a8d1113eeeaf7db71750501874fcdb8088e09b13
artmkchmv <102462249+artmkchmv@users.noreply.github.com>
2024-11-29 12:43:19 +0400

    Update MO_Lab_3.cpp

--- Lab3/MO_Lab_3.cpp
+++ Lab3/MO_Lab_3.cpp
@@ -9,36 +9,53 @@ int main(int argc, char* argv[]) {
 
 	numerics::vector_f64 x_start = { 0, 0 };
 
-	std::cout << "Gradient Descend method: " << grad_descend(test_func2d, x_start) << '\n';
+	std::cout << "Gradient Descend method:   " << grad_descend(test_func2d, x_start) << '\n';
 	std::cout << "Conjugate Gradient method: " << conjugate_grad(test_func2d, x_start) << '\n';
-	std::cout << "Newton_Raphson method: " << newton_raphson(test_func2d, x_start) << '\n';
+	std::cout << "Newton_Raphson method:     " << newton_raphson(test_func2d, x_start) << '\n';
 
-	using vect = numerics::vector_f64;
-	using bound_func = std::function<double(const vect&)>;
+    using vect = numerics::vector_f64;
+    using bound_func = std::function<double(const vect&)>;
 
-	bound_func target_function = [](const vect& args) {
-		return (args[0] - 1) * (args[0] - 1) + (args[1] - 1) * (args[1] - 1);
-		};
+    penalty_f64 penalty_func_no_penalty(test_func2d, 0);
 
-	penalty_f64 penalty_func(target_function);
+    penalty_f64 penalty_func_internal(test_func2d, 1, 1.0);
 
-	penalty_func.add_bound([](const vect& args) -> double {
-		return args[0] < 0 ? 100.0 : 0.0; // Штраф, если x < 0
-		});
+    penalty_func_internal.add_bound([](const vect& args) -> double {
+        return (args[0] - 5) * args[0] + (args[1] - 3) * args[1] - 0.25;
+        }); // (x - 5) * x + (y - 3) * y > 0.25
 
-	penalty_func.add_bound([](const vect& args) -> double {
-		return args[1] < 0 ? 100.0 : 0.0; // Штраф, если y < 0
-		});
+    penalty_f64 penalty_func_external(test_func2d, 2, 1.0);
 
-	penalty_func.add_bound([](const vect& args) -> double {
-		return args[0] > 10 ? 100.0 : 0.0; // Штраф, если x > 10
-		});
+    penalty_func_external.add_bound([](const vect& args) -> double {
+        return args[0] - 6; // x <= 6
+        });
 
-	penalty_func.add_bound([](const vect& args) -> double {
-		return args[1] > 10 ? 100.0 : 0.0; // Штраф, если y > 10
-		});
+    penalty_func_external.add_bound([](const vect& args) -> double {
+        return -args[0] - 1; // x >= -1
+        });
 
-	std::cout << "Penalty function method: " << penalty_func(x_start) << '\n';
+    penalty_func_external.add_bound([](const vect& args) -> double {
+        return 1 - args[1]; // y >= 1
+        });
+
+    penalty_func_external.add_bound([](const vect& args) -> double {
+        return 1 - args[1]; // y >= 1
+        });
+
+    std::cout << "\nPenalty function without bounds:\n";
+    std::cout << "Gradient Descend method:   " << grad_descend(penalty_func_no_penalty, x_start) << '\n';
+    std::cout << "Conjugate Gradient method: " << conjugate_grad(penalty_func_no_penalty, x_start) << '\n';
+    std::cout << "Newton_Raphson method:     " << newton_raphson(penalty_func_no_penalty, x_start) << '\n';
+
+    std::cout << "\nPenalty function with internal penalty:\n";
+    std::cout << "Gradient Descend method:   " << grad_descend(penalty_func_internal, x_start) << '\n';
+    std::cout << "Conjugate Gradient method: " << conjugate_grad(penalty_func_internal, x_start) << '\n';
+    std::cout << "Newton_Raphson method:     " << newton_raphson(penalty_func_internal, x_start) << '\n';
+
+    std::cout << "\nPenalty function with external penalty:\n";
+    std::cout << "Gradient Descend method:   " << grad_descend(penalty_func_external, x_start) << '\n';
+    std::cout << "Conjugate Gradient method: " << conjugate_grad(penalty_func_external, x_start) << '\n';
+    std::cout << "Newton_Raphson method:     " << newton_raphson(penalty_func_external, x_start) << '\n';
 
 	return 0;
 }
